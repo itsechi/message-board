f1c61b6cd65eebb72d80e88e039284e3be9366f9
Oliwia <itsechi@gmail.com>
2024-02-12 17:26:12 +0100

    Fix the form validation

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -18,8 +18,20 @@ exports.message_form_get = (req, res, next) => {
 
 // handle message form on POST
 exports.message_form_post = [
-  body("user", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
-  body("text", "Text must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("user")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Name cannot be empty")
+    .isLength({ max: 40 })
+    .withMessage("Name cannot be longer than 40 characters"),
+  body("text")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Text cannot be empty")
+    .isLength({ max: 500 })
+    .withMessage("Text cannot be longer than 500 characters"),
 
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
@@ -30,7 +42,7 @@ exports.message_form_post = [
     });
 
     // if there are errors render the form with error messages
-    if (!errors.isEmpty) {
+    if (!errors.isEmpty()) {
       res.render("form", {
         title: "Add a message - Mini Messageboard",
         errors: errors.array(),
--- models/message.js
+++ models/message.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-  user: { type: String, required: true, minLength: 1, maxLength: 25 },
+  user: { type: String, required: true, minLength: 1, maxLength: 40 },
   text: { type: String, required: true, minLength: 1, maxLength: 500 },
-  added: { type: String }
+  added: { type: String },
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
